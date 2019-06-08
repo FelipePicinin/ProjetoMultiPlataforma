ed38fd074f69b4cace9f18a5ab2972aa9edabda0
Felipe Picinin <felipepicinin10@gmail.com>
2019-06-07 21:14:41 -0300

    Commit projeto aula

---
+++ MultPlatProject/NewAuthorPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MultPlatProject
+{	
+	public partial class NewAuthorPage : ContentPage
+    {
+        async void Handle_Back(object sender, EventArgs e)
+        {
+            await DisplayAlert("Sucesso", "Cadastro realizado", "ok");
+            await Navigation.PopAsync();
+        }
+
+        void Handle_RequestFailed(object sender, MultPlatProject.ErrorResponse e)
+        {
+            DisplayAlert("Erro", e.Message, "ok");
+        }
+
+        public NewAuthorPage()
+		{
+            InitializeComponent();
+        }
+	}
+}
